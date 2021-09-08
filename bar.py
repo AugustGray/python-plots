d14d8cbc86180dc02f5723dd40856d05e19f57fa
AugustGray <hello.augustgray@gmail.com>
2021-09-07 21:08:01 -0600

    210907 - Bar test update

--- bar.py
+++ bar.py
@@ -1,12 +1,18 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-x = np.array(["A", "B", "C", "D"])
-y = np.array([3, 8, 1, 10])
-
-x2= np.array(["A", "B", "C", "D"])
-y2= np.array([2, 4, 0, 4])
-
-plt.bar(x,y)
-plt.bar(x2,y2)
+import numpy as np 
+import matplotlib.pyplot as plt 
+  
+X = ['Lunes','Martes','Miercoles','Jueves','Viernes','Sábado','Domingo']
+YSchoolOut = [14.5,14.5,12.5,9.5,17,0,0]
+ZWorkOut = [21.5,21.5,21.5,21.5,14,0,21.5]
+  
+X_axis = np.arange(len(X))
+  
+plt.bar(X_axis - 0.2, YSchoolOut, 0.4, label = 'School')
+plt.bar(X_axis + 0.2, ZWorkOut, 0.4, label = 'Work')
+  
+plt.xticks(X_axis, X)
+plt.xlabel("Days")
+plt.ylabel("Hours")
+plt.title("Weekly Schedule")
+plt.legend()
 plt.show()
